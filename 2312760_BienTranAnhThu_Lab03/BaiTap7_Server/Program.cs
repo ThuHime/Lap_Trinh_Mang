196def53fe78bb43a3e918db082ac3e6161786e9
Thurie <2312760@dlu.edu.vn>
2025-10-24 13:59:37 +0700

    Add files via upload

---
+++ 2312760_BienTranAnhThu_Lab03/BaiTap7_Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap7_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 5000);
+            server.Bind(ipep);
+
+            Console.WriteLine("Dang cho Client ket noi den...");
+
+            EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+            byte[] data = new byte[1024];
+            bool firstMsg = true;
+
+            while (true)
+            {
+                int recv = server.ReceiveFrom(data, ref remote);
+                string text = Encoding.ASCII.GetString(data, 0, recv);
+
+                if (firstMsg)
+                {
+                    Console.WriteLine("Thong diep duoc nhan tu " + remote.ToString() + ":");
+                    firstMsg = false;
+                }
+
+                Console.WriteLine(text);
+
+                string response = "xin chao";
+                byte[] resp = Encoding.ASCII.GetBytes(response);
+                server.SendTo(resp, remote);
+            }
+        }
+    }
+}
